b7c6626f755dd66eecfef60e963302ee1914d8b1
Xiang Lin <79429595+xianglin226@users.noreply.github.com>
2021-06-11 09:55:10 -0400

    Update Simulation.R

--- Simulation/Simulation.R
+++ Simulation/Simulation.R
@@ -14,7 +14,7 @@ zigamma <- function(n, p0=0.5, u=0.1, s=0.5, seed=0) {
 }
 
 #General zero inflated normal data
-zinormal<- function(n, p0=0.5, u=0.1, s=0.5, seed=0) {
+zinormal <- function(n, p0=0.5, u=0.1, s=0.5, seed=0) {
   seed=rep(0,n)
   v = s^2
   u <- u*10
@@ -22,21 +22,6 @@ zinormal<- function(n, p0=0.5, u=0.1, s=0.5, seed=0) {
   for (i in 1:n) { 
     if (runif(1, min=0, max=1)>p0)
     {seed[i]=rnorm(1, mean=u, sd=sqrt(v))}
-    else
-    {seed[i]=0}
-  }
-  seed/10
-}
-
-#Generate gamma data
-zigamma <- function(n, p0=0, u=0.1, s=0.5, seed=0) {
-  seed=rep(0,n)
-  v = s^2
-  u <- u*10
-  v <- v*10
-  for (i in 1:n) { 
-    if (runif(1, min=0, max=1)>p0)
-    {seed[i]=rgamma(n=1,shape=u^2/v,rate=u/v)}
     else
     {seed[i]=0}
   }
